63aceaae4594eafcd8d9d2c54cf3d7803fd784af
Lee Moody <lee@leemoody.com>
2024-03-15 11:01:22 -0600

    Section 5: added parent/child communication, observables

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent {
+  model: any = {};
+  constructor(public accountService: AccountService) {}
+  ngOnInit(): void {}
+
+  login(): void {
+    // when the http service call is complete, it is auto unsubscribed because it is elminated from memory
+    this.accountService.login(this.model).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+}
