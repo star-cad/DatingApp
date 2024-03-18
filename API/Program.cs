841d11aedef82f7f21849537113a847463f3828c
Lee Moody <lee@leemoody.com>
2024-03-18 10:55:07 -0600

    End of section 8

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,17 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
+
+/*
+
+    IMPORTANT: Order of operations below is significant.
+
+*/
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
@@ -25,4 +32,18 @@ app.UseAuthorization(); // you have a valid token, now do you want to do
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 app.Run();
