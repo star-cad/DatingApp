9a45d63606d0c72b5623d261d9b55818fdd83eff
Lee Moody <lee@leemoody.com>
2024-05-12 14:17:29 -0600

    End of Section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using API.DTOs;
+﻿using System.Security.Claims;
+using API.DTOs;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -30,6 +31,23 @@ public class UsersController : BaseApiController
     {
         return await _userRepository.GetMemberAsync(username);
     }
+
+    [HttpPut] // /api/username
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _userRepository.GetUserByUserNameAsync(username);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _mapper.Map(memberUpdateDto, user);
+        if (await _userRepository.SaveAllAsync()) { return NoContent(); }
+
+        return BadRequest("Failed to update user.");
+    }
+
 
     // [HttpGet("{id}")] // /api/users/#
     // public async Task<ActionResult<AppUser>> GetUser(int id)
